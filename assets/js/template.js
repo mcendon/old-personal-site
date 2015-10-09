f3061b7c41d4f0175676da09c1360a9189b71ded
Mauro Cendon <mau.cendon@gmail.com>
2015-10-09 02:22:13 -0300

    fix js background

--- assets/js/template.js
+++ assets/js/template.js
@@ -2,7 +2,7 @@ setInterval(function () {
 	var pos = $("#head").css("backgroundPosition").split(" ");
 	var posX = parseFloat(pos[0]);
 	if (Math.abs(posX) == $("#head").width())
-		pos = 0;
+		posX = 0;
 
 	$("#head").css("backgroundPosition", (posX - 1) + "px " + pos[1]);
 }, 50);
