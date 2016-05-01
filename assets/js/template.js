b66f21dd2155b5f97d11ae61179dd4573d031e0e
Mauro Cendon Hidalgo <mau.cendon@gmail.com>
2016-05-01 01:06:42 -0300

    Personal page facewash

--- assets/js/template.js
+++
@@ -1,8 +0,0 @@
-setInterval(function () {
-	var pos = $("#head").css("backgroundPosition").split(" ");
-	var posX = parseFloat(pos[0]);
-	if (Math.abs(posX) == $("#head").width())
-		posX = 0;
-
-	$("#head").css("backgroundPosition", (posX - 1) + "px " + pos[1]);
-}, 50);
--- portabanner/public_html/new/scripts/portabanner.js
+++ portabanner/public_html/new/scripts/portabanner.js
@@ -1,4 +1,68 @@
+var pb = {
+  initialize: function() {
+    pb.products = [{"title": "Producto 1",
+     "sku": "RU-85E",
+     "image": "img/roll-up-banner-1.jpg",
+     "price": 150.52,
+     "short_description": "ESTRUCTURA ROLLUP ECO 0.85 x 2.00 METROS",
+     "long_description": "StandPortátil® 1 el sistema ideal para gráficas de gran formato. \
+      Este producto es compacto, portable y extremadamente fácil de armar.\
+      Se puede configurar de forma lineal, curva, cóncava o convexa. \
+      La manera más elegante y profesional de exponer sin uniones gráficas, \
+      imágenes de gran tamaño. Los textos y las gráficas no estarán interrumpidas por \
+      costuras y serán mucho más atractivas."},
+      {"title": "Producto 2",
+       "sku": "RU-95E",
+       "image": "img/roll-up-banner-1.jpg",
+       "price": 200.00,
+       "short_description": "ESTRUCTURA ROLLUP ECO 0.85 x 2.00 METROS",
+       "long_description": "StandPortátil® 2 el sistema ideal para gráficas de gran formato. \
+        Este producto es compacto, portable y extremadamente fácil de armar.\
+        Se puede configurar de forma lineal, curva, cóncava o convexa. \
+        La manera más elegante y profesional de exponer sin uniones gráficas, \
+        imágenes de gran tamaño. Los textos y las gráficas no estarán interrumpidas por \
+        costuras y serán mucho más atractivas."},
+        {"title": "Producto 3",
+         "sku": "RU-105E",
+         "image": "img/roll-up-banner-1.jpg",
+         "price": 1000.50,
+         "short_description": "ESTRUCTURA ROLLUP ECO 0.85 x 2.00 METROS",
+         "long_description": "StandPortátil® 3 el sistema ideal para gráficas de gran formato. \
+          Este producto es compacto, portable y extremadamente fácil de armar.\
+          Se puede configurar de forma lineal, curva, cóncava o convexa. \
+          La manera más elegante y profesional de exponer sin uniones gráficas, \
+          imágenes de gran tamaño. Los textos y las gráficas no estarán interrumpidas por \
+          costuras y serán mucho más atractivas."}];
+  },
+  products: [],
+  navigation: {
+    go: function(productNumber) {
+      var product = pb.products[productNumber-1];
+      $("div.product-description").fadeOut(500, function() {
+        $(this).html(product.long_description)
+        .fadeIn()
+      });
+
+      $("div.product-title h2").fadeOut(500, function() {
+        $(this).html(product.sku + "&nbsp")
+        .append($("<small>").text(product.short_description))
+        .fadeIn();
+      });
+
+      $("div.product-price").fadeOut(500, function() {
+        $(this).html("DESDE")
+        .append($("<strong>").text(" u$d ").append(product.price))
+        .append(" + IVA")
+        .fadeIn();
+      });
+
+    }
+  }
+}
+
+
 $(document).ready(function(){
+  pb.initialize(); //inicializo portabanner
 
   $(".product-form").on("submit", function(e) {
     e.preventDefault();
@@ -28,11 +92,6 @@ $(document).ready(function(){
             $(this).show();
         });
     });
-  });
-
-  $('#fullpage').fullpage({
-      responsiveWidth:1000,
-      responsiveHeight: 500
   });
 
 });
